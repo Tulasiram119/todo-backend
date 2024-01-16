f342e58f42801e4b21028a8803b734f4e5ff321c
Tulasiram <tulasiramkancharla119@gmail.com>
2024-01-16 09:56:40 +0530

    Updated Cors policy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDb = require("./db");
 const mongoose = require("mongoose");
 const errorHandler = require("./errorHandler");
+const cors = require("cors");
 
 require("dotenv").config();
 const app = express();
@@ -9,6 +10,7 @@ const PORT = 3500 || process.env.PORT;
 
 connectDb();
 app.use(express.json());
+app.use(cors());
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
 app.use("/todo", require("./routes/todo"));
